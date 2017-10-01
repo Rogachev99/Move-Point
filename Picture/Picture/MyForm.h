616257ed582410de504ba8fca96fb2c784a5978e
Rogachev99 <32433285+Rogachev99@users.noreply.github.com>
2017-10-01 17:39:48 +0300

    #V 0.0.1
    #V 0.0.1
    1) Память используется не оптимально (нет динамической памяти).

---
+++ Picture/Picture/MyForm.h
@@ -0,0 +1,225 @@
+#pragma once
+//массив координат точки
+int arr_X[10000]; 
+int arr_Y[10000];
+
+namespace Picture {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+	/// <summary>
+	/// Сводка для MyForm
+	/// </summary>
+	public ref class MyForm : public System::Windows::Forms::Form
+	{
+
+	public:
+		MyForm(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: добавьте код конструктора
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Освободить все используемые ресурсы.
+		/// </summary>
+		~MyForm()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	private: System::Windows::Forms::Button^  button1;
+	private: System::Windows::Forms::Button^  button2;
+	private: System::Windows::Forms::PictureBox^  pictureBox1;
+	private: System::Windows::Forms::PictureBox^  pictureBox2;
+	private: System::Windows::Forms::Label^  label1;
+	private: System::Windows::Forms::Timer^  timer1;
+	private: System::Windows::Forms::Timer^  timer2;
+	private: System::ComponentModel::IContainer^  components;
+
+	protected: 
+
+	private:
+		/// <summary>
+		/// Требуется переменная конструктора.
+		/// </summary>
+
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Обязательный метод для поддержки конструктора - не изменяйте
+		/// содержимое данного метода при помощи редактора кода.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->components = (gcnew System::ComponentModel::Container());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->pictureBox1 = (gcnew System::Windows::Forms::PictureBox());
+			this->pictureBox2 = (gcnew System::Windows::Forms::PictureBox());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->timer1 = (gcnew System::Windows::Forms::Timer(this->components));
+			this->timer2 = (gcnew System::Windows::Forms::Timer(this->components));
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^  >(this->pictureBox1))->BeginInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^  >(this->pictureBox2))->BeginInit();
+			this->SuspendLayout();
+			// 
+			// button1
+			// 
+			this->button1->Location = System::Drawing::Point(318, 12);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(75, 23);
+			this->button1->TabIndex = 0;
+			this->button1->Text = L"START";
+			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &MyForm::button1_Click);
+			// 
+			// button2
+			// 
+			this->button2->Location = System::Drawing::Point(318, 41);
+			this->button2->Name = L"button2";
+			this->button2->Size = System::Drawing::Size(75, 23);
+			this->button2->TabIndex = 2;
+			this->button2->Text = L"STOP";
+			this->button2->UseVisualStyleBackColor = true;
+			this->button2->Click += gcnew System::EventHandler(this, &MyForm::button2_Click);
+			// 
+			// pictureBox1
+			// 
+			this->pictureBox1->BackColor = System::Drawing::SystemColors::Window;
+			this->pictureBox1->BorderStyle = System::Windows::Forms::BorderStyle::FixedSingle;
+			this->pictureBox1->Location = System::Drawing::Point(12, 12);
+			this->pictureBox1->Name = L"pictureBox1";
+			this->pictureBox1->Size = System::Drawing::Size(300, 300);
+			this->pictureBox1->TabIndex = 3;
+			this->pictureBox1->TabStop = false;
+			this->pictureBox1->MouseDown += gcnew System::Windows::Forms::MouseEventHandler(this, &MyForm::pictureBox1_MouseDown);
+			this->pictureBox1->MouseMove += gcnew System::Windows::Forms::MouseEventHandler(this, &MyForm::pictureBox1_MouseMove);
+			this->pictureBox1->MouseUp += gcnew System::Windows::Forms::MouseEventHandler(this, &MyForm::pictureBox1_MouseUp);
+			// 
+			// pictureBox2
+			// 
+			this->pictureBox2->BackColor = System::Drawing::Color::Black;
+			this->pictureBox2->Enabled = false;
+			this->pictureBox2->Location = System::Drawing::Point(300, 130);
+			this->pictureBox2->Name = L"pictureBox2";
+			this->pictureBox2->Size = System::Drawing::Size(3, 3);
+			this->pictureBox2->TabIndex = 4;
+			this->pictureBox2->TabStop = false;
+			this->pictureBox2->Visible = false;
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Location = System::Drawing::Point(315, 297);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(21, 13);
+			this->label1->TabIndex = 5;
+			this->label1->Text = L"XY";
+			// 
+			// timer1
+			// 
+			this->timer1->Enabled = true;
+			this->timer1->Interval = 20;
+			this->timer1->Tick += gcnew System::EventHandler(this, &MyForm::MyForm1_Load);
+			// 
+			// timer2
+			// 
+			this->timer2->Interval = 10;
+			this->timer2->Tick += gcnew System::EventHandler(this, &MyForm::Move_Load);
+			// 
+			// MyForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(397, 320);
+			this->Controls->Add(this->label1);
+			this->Controls->Add(this->pictureBox2);
+			this->Controls->Add(this->pictureBox1);
+			this->Controls->Add(this->button1);
+			this->Controls->Add(this->button2);
+			this->FormBorderStyle = System::Windows::Forms::FormBorderStyle::FixedSingle;
+			this->Name = L"MyForm";
+			this->Text = L"MyForm";
+			this->Load += gcnew System::EventHandler(this, &MyForm::MyForm_Load);
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^  >(this->pictureBox1))->EndInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^  >(this->pictureBox2))->EndInit();
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
+		}
+#pragma endregion
+		unsigned short k,i;
+		bool Drow;
+		Point p;
+		Graphics ^ MyGraphics;		//Объект Graphics для рисования
+		Pen ^ MyPen;				//Перо для рисования
+		Bitmap^ bitmap;
+
+//================Инициализация_переменных=========================================
+		private: System::Void MyForm_Load(System::Object^  sender, System::EventArgs^  e) {
+			k = 0;
+			MyPen = gcnew Pen(Color::Red,1);
+			bitmap = gcnew Bitmap(pictureBox1->Width, pictureBox1->Height);
+			pictureBox1->Image = bitmap;
+			MyGraphics = Graphics::FromImage(bitmap);
+		}
+//================Перерисовка окна и вывод кол-ва элементов массива==================
+		private: System::Void MyForm1_Load(System::Object^  sender, System::EventArgs^  e) {
+			pictureBox1->Image = bitmap;
+			label1->Text = k.ToString();
+			if (k >= 9900) k=0;
+		}
+//================Перемещение точки===================================================
+		private: System::Void Move_Load(System::Object^  sender, System::EventArgs^  e){
+			pictureBox2->Location = System::Drawing::Point(arr_X[i], arr_Y[i]);
+			if (i!=k) i++;
+			else i=0;
+		}
+//================START================================================================
+		private: System::Void button1_Click(System::Object^  sender, System::EventArgs^  e) {
+			i=0;
+			timer2->Enabled = true;
+			pictureBox2->Visible = true;
+		}
+//================STOP=================================================================
+		private: System::Void button2_Click(System::Object^  sender, System::EventArgs^  e) {
+			timer2->Enabled = false;
+			MyGraphics->Clear(Color::White);
+			pictureBox2->Visible = false;
+			k=0;
+		}
+//=====================================================================================
+		private: System::Void pictureBox1_MouseMove(System::Object^ sender, System::Windows::Forms::MouseEventArgs^ e) {
+			if (Drow == true) {
+				if (p.X != e->X || p.Y != e->Y) {
+					MyGraphics->DrawLine(MyPen, p.X, p.Y, e->X, e->Y);
+					arr_X[k] = p.X+12;
+					arr_Y[k] = p.Y+12;
+					k++;
+					p.X = e->X; p.Y = e->Y;
+				}
+			}
+		}
+//=====================================================================================
+		private: System::Void pictureBox1_MouseDown(System::Object^ sender, System::Windows::Forms::MouseEventArgs^ e) {
+			Drow = true;
+			p.X = e->X; p.Y = e->Y;
+		}
+//=====================================================================================
+		private: System::Void pictureBox1_MouseUp(System::Object^ sender, System::Windows::Forms::MouseEventArgs^ e) {
+			Drow = false;
+			p.X = e->X; p.Y = e->Y;			//!!!!хз для чего оно здесь
+		}
+//=====================================================================================
+	};
+}
